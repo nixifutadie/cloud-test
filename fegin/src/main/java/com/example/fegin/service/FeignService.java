8beb6e59c25996f6b6e1257d5d03c9164e41e8d5
liuchunjun <liuchunjun@whzjkj.net>
2019-06-28 17:17:28 +0800

    second-commit

--- fegin/src/main/java/com/example/fegin/service/FeignService.java
+++ fegin/src/main/java/com/example/fegin/service/FeignService.java
@@ -1,7 +1,7 @@
 package com.example.fegin.service;
 
 import com.example.fegin.entity.User;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "hello-service",fallback = FeignFallBack.class)
